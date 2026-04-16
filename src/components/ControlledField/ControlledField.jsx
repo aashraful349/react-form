9f1e0bb5b76b6ce2ba9814f652ff8bb7c19ffe39
aashraful349 <aashraful349@gmail.com>
2026-04-16 12:09:05 +0600

    react-form-practice

---
+++ src/components/ControlledField/ControlledField.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ControlledField = () => {
+    return (
+        <div>
+            <p>fuck you</p>
+        </div>
+    );
+};
+
+export default ControlledField;
